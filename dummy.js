208fab809851ac9aa5cbb64a05903d80ffc0e4f9
Rich Reneau <rich.reneau@gmail.com>
2018-02-21 20:45:56 -0500

    modifications to query on workouts route

--- dummy.js
+++ dummy.js
@@ -6,55 +6,43 @@ const Routine_exercise = require('./models/routine_exercise')
 const Workouts = require('./models/workouts');
 const Actuals = require('./models/actuals');
 
-Exercises.create({
-    exerciseName: 'Push-ups',
-    picPath: './strengthApp/pics/',
-    videoPath: './strengthApp/video/',
-    instructions: "Starting Position: Kneel on an exercise mat or flooher behind you..."
-}).then (exercise => {
-        Routines.create({
-            routineName: 'Strength with Calisthenics'
-        }).then (routine => {
+// Routines.create({
+//     routineName: 'Basic Strength'
+// })
+
+Routines.findOne({where: {routineName: 'Basic Strength'}
+    }).then (routine => {
+        Exercises.create({
+            exerciseName: 'Back Extension',
+            picPath: './strengthApp/pics/',
+            videoPath: './strengthApp/video/',
+            instructions: "instructions for Back Extension..."
+        }).then (exercise => {
             Routine_exercise.create({
-                orderNum: 1,
+                orderNum: 2,
                 weight: 'bw',
                 sets: 2,
                 reps: 20,
                 routineId: routine.id,
                 exerciseId: exercise.id
-            }).then (routine_exercise => {
-                Workouts.create({
-                    date: '2018-02-20',
-                    routineId: routine.id
-                }).then (workout => {
-                    Actuals.create({
-                        setNum: 1,
-                        actualReps: 15,
-                        actualWgt: 'bw',
-                        excersiseId: routine_exercise.excersiseId,
-                        workoutId: workout.id
-                    })  
-                });
-            });
+            })
         });
-    })
+    });
 
-// Routines.findById(1).then (routine => {
-//     Workouts.create({
-//         date: '2018-02-20'
-//     }) 
-// })
 
-// Workouts.findById(1).then (workout => {
-//     Routines.findById(1).then(routine => {
-//         Routine_exercise.findById(1).then(routine_exercise => {
-//             Actuals.create({
-//                 setNum: 1,
-//                 actualReps: 15,
-//                 actualWgt: 'bw',
-//                 excersiseId: routine_exercise.excersiseId,
-//                 workoutId: workout.id
-//             })
-//         })
-//     })
-// })
+    // .then (routine_exercise => {
+            //     Workouts.create({
+            //         date: '2018-02-20',
+            //         routineId: routine.id
+            //     }).then (workout => {
+            //         Actuals.create({
+            //             setNum: 1,
+            //             actualReps: 15,
+            //             actualWgt: 'bw',
+            //             excersiseId: routine_exercise.excersiseId,
+            //             workoutId: workout.id
+            //         })  
+            //     });
+            // });
+        //  });
+
--- routes/workouts.js
+++ routes/workouts.js
@@ -1,29 +1,46 @@
 var express = require('express');
 var router = express.Router();
+const Sequelize = require('sequelize');
+const sequelize = require('../db');
 
 const Workouts = require('../models/workouts');
 const Actuals = require('../models/actuals');
+const Routines = require('../models/routines');
+const Routine_exercise = require('../models/routine_exercise');
+const Exercises = require('../models/exercises');
 
 router.route('/')
     .get((req, res) => {
-        console.log('Its working');
-        Workouts.findAll()
-        .then(allWorkouts => {
-            res.json(allWorkouts);
-        });
-    })
+        console.log('Get is working');
+        Routines.findOne({where: {active: true} })
+        .then (routine => {
+            console.log('The routine Id is: ' + routine.id);
+            Routine_exercise.findAll({where: {routineId: routine.id} })
+            .then (results => {
+                console.log('Dev is working!!!')
+                res.json(results);
+            })
+        })
+    }) 
+    // .get((req, res) => {
+    //     console.log('Get is working');
+    //     Routine_exercise.findAll() 
+    //     .then (results => {
+    //         res.json(results);
+    //     })
+    // })
     .post((req, res) => {
 
         console.log(req.body);
 
         Workouts.create({
-        date: req.body.date
+        date: req.body.date,
+        routineId: req.body.routineId
         }).then(workout => {
             Actuals.create({
                 setNum: req.body.setNum,
                 actualReps: req.body.actualReps,
                 actualWgt: req.body.actualWgt,
-                routineId: req.body.routineId,
                 exerciseId: req.body.exerciseId,
                 workoutId: workout.id
             })
