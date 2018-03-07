79ffb801338d3130e9bfcd33121b3e85ab985650
Rich Reneau <rich.reneau@gmail.com>
2018-03-07 15:50:24 -0500

    finished styling for features section

--- routes/results.js
+++ routes/results.js
@@ -11,16 +11,30 @@ const Routine_exercise = require('../models/routine_exercise');
 const Exercises = require('../models/exercises');
 
 router.get('/', function(req, res, next) {
+    wrkAll = [];
     var d = new Date();
     d.setDate(d.getDate() - 7);
     Workouts.findAll({where: {date: {[Op.gt]: d}} 
     }).then (workouts => {
-        // workouts.forEach(workout => {
-        //     Actuals.findAll({where: {workoutId: workout.id} })
-        // })
-            res.render('results', {workouts});   
+        workouts.forEach(workout => {
+            Actuals.findAll({where: 
+                {workoutId: workout.id} 
+            }).then (actuals => {
+                let actualsArray = actuals.map(actual => {
+                    Exercises.findOne({where: {id: actual.exerciseId} })
+                    .then (exercise => {
+                        return {
+                            weight: actual.actualWgt,
+                            exerciseName: exercise.exerciseName
+                        }
+                    })
+                })
+            })
+
         })
-        
-  });
+            res.render('results', {workouts});
+               
+    })
+});
   
 module.exports = router;
