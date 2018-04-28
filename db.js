bb2f2785d13603df749ed9fd0e0ab1d7fc403201
Rich Reneau <rich.reneau@gmail.com>
2018-04-28 07:04:41 -0400

    established hidden environment variables for the DB

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize('strengthApp-DB', 'richreneau', '', {
-  host: 'localhost',
+const sequelize = new Sequelize('strengthApp-DB', process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.HOST,
   dialect: 'postgres'
 });
 
